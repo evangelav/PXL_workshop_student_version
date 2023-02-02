65c9b120a0d97e62a3551686de10e5dd7be8184b
evangelav <evangelav16@gmail.com>
2023-02-02 11:07:32 +0100

    bug fix

--- wordpair_app/views.py
+++ wordpair_app/views.py
@@ -10,6 +10,7 @@ from wordpair_app.serializers import WordSerializer
 def word_list(request):
      if request.method == 'GET':
         words = Word.objects.all()
+        
         serializer = WordSerializer(words, many=True)
         data = {"words" : serializer.data}
         return Response(data, status=status.HTTP_200_OK)
